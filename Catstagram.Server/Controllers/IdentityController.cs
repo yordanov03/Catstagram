3ceeeee6e46e599abd6243295dbddd3d21ee0fdf
yordanov03 <svetoslav.yordanov.003@gmail.com>
2022-09-11 19:41:17 +0200

    changed login controller to return token (#2)

--- Catstagram.Server/Controllers/IdentityController.cs
+++ Catstagram.Server/Controllers/IdentityController.cs
@@ -39,7 +39,7 @@ namespace Catstagram.Server.Controllers
         }
 
         [Route(nameof(Login))]
-        public async Task<ActionResult<string>> Login(LoginRequestModel model)
+        public async Task<ActionResult<object>> Login(LoginRequestModel model)
         {
             var user = await this.userManager.FindByNameAsync(model.Username);
 
@@ -65,7 +65,7 @@ namespace Catstagram.Server.Controllers
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var encryptedToken =  tokenHandler.WriteToken(token);
 
-            return encryptedToken;
+            return new { Token = encryptedToken };
         }
     }
 }
