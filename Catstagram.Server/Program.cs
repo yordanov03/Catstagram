cede8a0deb0c65e5ff897fbb139a6beb3cb1b76b
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2022-09-13 13:10:14 +0200

    removed swagger as initial page

--- Catstagram.Server/Program.cs
+++ Catstagram.Server/Program.cs
@@ -17,11 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseMigrationsEndPoint()
     .UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseRouting();
